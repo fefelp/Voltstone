120285d3a6b281f6a897023843783de4d052321b
fefelp <fefelp_jefferson@yahoo.com>
2025-07-29 07:10:54 -0300

    Atualizar o utils.js

--- utils.js
+++ utils.js
@@ -1,20 +1,68 @@
-// utils.js
+const fs = require('fs');
 
-function formatarValor(valor) {
-  return parseFloat(valor).toFixed(2) + ' USDT';
+const file = 'database.json';
+
+function carregar() {
+  if (!fs.existsSync(file)) fs.writeFileSync(file, '{}');
+  return JSON.parse(fs.readFileSync(file));
 }
 
-function formatarData(dataISO) {
-  const data = new Date(dataISO);
-  return data.toLocaleDateString('pt-BR');
+function salvar(data) {
+  fs.writeFileSync(file, JSON.stringify(data, null, 2));
 }
 
-function calcularRendimento(valor, percentual) {
-  return parseFloat((valor * (percentual / 100)).toFixed(2));
+function addUser(id, username, nome) {
+  const db = carregar();
+  if (!db[id]) db[id] = { nome, username, valor: 0, rendimento: 0, historico: [] };
+  salvar(db);
+}
+
+function getUser(id) {
+  const db = carregar();
+  return db[id];
+}
+
+function salvarCarteira(id, carteira) {
+  const db = carregar();
+  if (!db[id]) return;
+  db[id].carteira = carteira;
+  salvar(db);
+}
+
+function getCarteira(id) {
+  const db = carregar();
+  return db[id]?.carteira;
+}
+
+function registrarDeposito(id, valor) {
+  const db = carregar();
+  if (!db[id]) return;
+  db[id].valor += valor;
+  salvar(db);
+}
+
+function getHistorico(id) {
+  const db = carregar();
+  return db[id]?.historico || [];
+}
+
+function getTotalInvestido() {
+  const db = carregar();
+  return Object.values(db).reduce((soma, u) => soma + (u.valor || 0), 0);
+}
+
+function getTotalUsuarios() {
+  const db = carregar();
+  return Object.keys(db).length;
 }
 
 module.exports = {
-  formatarValor,
-  formatarData,
-  calcularRendimento
+  addUser,
+  getUser,
+  salvarCarteira,
+  getCarteira,
+  registrarDeposito,
+  getHistorico,
+  getTotalInvestido,
+  getTotalUsuarios
 };
