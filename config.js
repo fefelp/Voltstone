5e2de1101d2a4b877150d9f5b6c33cdbf6816140
fefelp <fefelp_jefferson@yahoo.com>
2025-08-03 12:49:44 -0300

    Criar config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // 🔐 Token real do Bot fornecido por você (NÃO compartilhe isso publicamente)
+  botToken: "7808081125:AAFId0bjTAOumeFWc1SjBSNosK6EoetkPiQ",
+
+  // 👤 Seu ID de administrador no Telegram
+  adminId: 5608086275,
+
+  // 💳 Endereço TRC-20 para onde os usuários devem enviar USDT
+  carteiraDeposito: "TVmzjRPgjYt7c1E59z6AtG9U2kZYKex4JZ",
+
+  // 💰 Valor mínimo de depósito permitido
+  depositoMinimo: 50
+};
