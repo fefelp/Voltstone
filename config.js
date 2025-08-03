12b942c3c9881be07bbb9a0ff3771f2cc554fcbc
fefelp <fefelp_jefferson@yahoo.com>
2025-08-03 13:22:13 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
+// config.js
 module.exports = {
   botToken: process.env.BOT_TOKEN,
-  adminId: Number(process.env.ADMIN_ID), // cuidado que é número
+  adminId: Number(process.env.ADMIN_ID),
   carteiraDeposito: process.env.CARTEIRA_DEPOSITO,
   depositoMinimo: Number(process.env.DEPOSITO_MINIMO) || 50
 };
