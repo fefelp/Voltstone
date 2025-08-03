d2deb06ea44268d00fcf5e3393ca66754630c789
fefelp <fefelp_jefferson@yahoo.com>
2025-08-03 13:33:38 -0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-module.exports = {
-  botToken: process.env.BOT_TOKEN,
-  adminId: Number(process.env.ADMIN_ID),
-  carteiraDeposito: process.env.CARTEIRA_DEPOSITO,
-  depositoMinimo: Number(process.env.DEPOSITO_MINIMO) || 50
-};
